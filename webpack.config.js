4ac6b5dbfa4f031555b3a0bb8180e35aaf114d5c
lsk <longsk@casc.ac.cn>
2020-11-25 17:03:16 +0800

    解决yarn  安装失败的问题 禁用图片压缩插件 imagemin-webpack-plugin 安装 https://www.yuque.com/easy-team/easywebpack/problem#c0325d69

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
 'use strict';
 module.exports = {
-  egg: true,
-  framework: 'vue', // 使用 easywebpack-vue 构建解决方案
-  entry: {
-    app: 'app/web/index.js'
-  },
-  plugins: [{
-    // copy-webpack-plugin
-   /* copy: [{
-      from: 'app/web/asset/css/bootstrap.min.css',
-      to: 'asset/css/bootstrap.min.css'
-    }]*/
-  }],
-  /*scss: {
-    options: { includePaths: ["app/web/style"] }
-  },
-  module:{
-    rules:[
-      {
-        test: /\.scss$/,
-        use: [
-          'css-loader',
-          'postcss-loader',// 添加css前缀
-          'sass-loader',
-        ]
-      },
-    ]
-  },*/
+    egg: true,
+    framework: 'vue', // 使用 easywebpack-vue 构建解决方案
+    entry: {
+        app: 'app/web/index.js'
+    },
+    plugins: [
+        {imagemini: false},  // 禁用图片压缩插件 imagemin-webpack-plugin 安装
+        {
+            // copy-webpack-plugin
+            /* copy: [{
+               from: 'app/web/asset/css/bootstrap.min.css',
+               to: 'asset/css/bootstrap.min.css'
+             }]*/
+        }],
+    /*scss: {
+      options: { includePaths: ["app/web/style"] }
+    },
+    module:{
+      rules:[
+        {
+          test: /\.scss$/,
+          use: [
+            'css-loader',
+            'postcss-loader',// 添加css前缀
+            'sass-loader',
+          ]
+        },
+      ]
+    },*/
 };
